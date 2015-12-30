09228518ecf528ef1e7c05dcad2d3add05c8c74c
sharpmental <shunmin.wu@gmail.com>
2015-12-30 16:18:53 +0800

    update

--- vhost/application/views/adminpanel/dep_locarea_param/modify.php
+++ vhost/application/views/adminpanel/dep_locarea_param/modify.php
@@ -5,7 +5,7 @@
 
 	<div class='panel panel-default'>
 		<div class='panel-heading'>
-			<i class='glyphicon glyphicon-edit'></i> 修改定位参数
+			<i class='glyphicon glyphicon-edit'></i> 修改部门定位参数
 			<div class='panel-tools'>
 				<div class='btn-group'>
 				<?php aci_ui_a($folder_name,'edittable','index','',' class="btn  btn-sm pull-right"','<span class="glyphicon glyphicon-arrow-left"></span> 返回')?>
